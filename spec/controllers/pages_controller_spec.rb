763bd8e752679fccaa35374d7312afea141bd2bf
dreummeur <dreummeur@gmail.com>
2010-08-18 06:19:45 +0200

    Really done with static pages. Well, almost. Probably.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,10 @@ describe PagesController do
 
   integrate_views
 
+  before(:each) do
+    @base_title = "Mondegreen | "
+  end
+
   describe "GET 'home'" do
 
     it "should be successful" do
@@ -13,7 +17,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_tag("title", "Mondegreen | Home")
+      response.should have_tag("title", @base_title + "Home")
     end
     
   end
@@ -26,7 +30,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_tag("title", "Mondegreen | Contact")
+      response.should have_tag("title", @base_title + "Contact")
     end
 
   end
@@ -39,7 +43,21 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_tag("title", "Mondegreen | About")
+      response.should have_tag("title", @base_title + "About")
+    end
+
+  end
+
+  describe "GET 'help'" do
+
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title", @base_title + "Help")
     end
 
   end
